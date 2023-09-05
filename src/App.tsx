cfac468c10fdb2d387eb7efe1afd92db8cfaf764
Opeyemi-Omotayo <Opeyemi.Motayo1@gmail.com>
2023-09-05 14:50:17 +0100

    feat: installed react date-time

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
+    <div className="bg-red-500">
       <h1>Hello world </h1>
     </div>
   );
