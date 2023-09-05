85560ff4888bd718bee821522dbffce1cd929914
Opeyemi-Omotayo <Opeyemi.Motayo1@gmail.com>
2023-09-05 18:46:10 +0100

    feat:task component

---
+++ src/components/tasks/Tasks.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Tasks = () => {
+  return (
+    <div>
+        <h1>My Tasks</h1>
+        <div >
+            <div >
+            <input type="checkbox" name="" id="" className='mr-3'/>
+            <div>
+                <h1>Create Wireframe</h1>
+                <p>11:00 am - 11:30 am</p>
+            </div>
+            </div>
+            <div>
+                <h1>Today</h1>
+            </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Tasks;
